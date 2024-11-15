fb20a8fcaeb9070955d8fff8f0f82444967a3fef
Pavel-Burdakov <p.o.burdakov@yandex.ru>
2024-11-15 09:48:47 +0300

    first commit

---
+++ src/main/java/com/example/auth/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.auth;
+// src/main/java/com/example/demo/security/SecurityConfig.java
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+}
+
+
+
+
+
